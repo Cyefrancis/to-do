ab68627453c72e1b802a2fddcb14753887853a06
Francisco Navarrete <frncisco1243@gmail.com>
2024-07-18 20:44:05 -0400

    created task model, added input and button to main

--- AppToDo/models.py
+++ AppToDo/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=255)
-    
+
+    def __str__(self):
+        return self.title
