2a7022e7ff1c4a4ebcaf86dfe418bb6883e47d48
Francisco Navarrete <frncisco1243@gmail.com>
2024-07-19 22:07:10 -0400

    added styles and views for task

--- AppToDo/views.py
+++ AppToDo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Task
+from django.views.generic import ListView
 # Create your views here.
 
-def main(request):
-    return render(request,'main.html',{})
+class TaskList(ListView):
+    model = Task
+    template_name = 'main.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        return Task.objects.all()
--- ToDo/urls.py
+++ ToDo/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from AppToDo.views import main 
+from AppToDo.views import TaskList
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main, name='main')
+    path('', TaskList.as_view(), name='main')
 ]
