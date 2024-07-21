d85dfbd3221ff2dbb6cb0adcad6a67d7a2934717
Francisco Navarrete <frncisco1243@gmail.com>
2024-07-20 20:20:09 -0400

    added functional form

--- AppToDo/forms.py
+++ AppToDo/forms.py
@@ -1,7 +1,22 @@
-from django.forms import forms, ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter task title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter task content', 'rows': 5}),
+        }
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if not title:
+            raise forms.ValidationError('Title is required')
+        return title
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content:
+            raise forms.ValidationError('Content is required')
+        return content
